7c18be87f8936452176b6cf2073eb39ee2e6b9c5
MahaMekdad <maha_mekdad@outlook.com>
2021-02-27 20:21:42 +0200

    redirection and delete contact

--- .idea/functionalities.js
+++ .idea/functionalities.js
@@ -3,7 +3,7 @@ var userPhone;
 
 function addContact() {
     var flag = false;
-    debugger
+    //debugger
     console.log("inside the add Contact");
     contactsArray.forEach(contact => {
         if (contact.phone == document.getElementById('phone').value) {
@@ -58,7 +58,8 @@ function drawContact(contact) {
     //console.log(contact.name + " , " + contact.phone + " inside draw")
     var li = document.createElement("li");
     var a = document.createElement("a");
-    a.setAttribute("href", `try.html?${contact.phone}`);
+    a.setAttribute("href", `try.html?x=${contact.phone}`);
+    a.setAttribute("target", "_self" );
     //a.setAttribute("onclick", `t(${contact.phone})`)
     var img = document.createElement("img");
     if (contact.gender == "male") {
@@ -112,4 +113,20 @@ function t(contactPhone){
     callBtn.name = "call";
     callBtn.value = "call";
     $('#contactInfo').append(img, deleteBtn, editBtn, callBtn);
+}
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+function deleteContact(contactPhone){
+    var contactIndex = contactsArray.findIndex(contact => contact.phone == contactPhone)
+    contactsArray.splice(contactIndex, 1);
+    window.localStorage.setItem("contacts", JSON.stringify(contactsArray));
+    window.location.href = "Test.html";
 }
